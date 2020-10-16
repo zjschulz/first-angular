3319bfa49fd39deea09e9aa5ed73cfff9b80ac77
zjschulz <zjschulz@gmail.com>
2020-10-16 12:33:50 +0000

    revert back to servers comps for learning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
