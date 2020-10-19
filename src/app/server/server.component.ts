8706b5461717b171cd78167268630e1641d2f68e
zjschulz <zjschulz@gmail.com>
2020-10-19 13:31:59 +0000

    practice dynamic styles with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  templateUrl: './server.component.html'
 })
 export class ServerComponent implements OnInit {
   serverId: number = 10;
@@ -13,9 +12,15 @@ export class ServerComponent implements OnInit {
     return this.serverStatus;
   }
 
-  constructor() { }
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'online';
+  }
 
   ngOnInit(): void {
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 
 }
