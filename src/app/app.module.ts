74fd6fca33c18aadf12c6e4bac459ee4bd2c71cf
zjschulz <zjschulz@gmail.com>
2020-10-19 13:22:30 +0000

    oractice using ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { UsernameComponent } from './username/username.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    UsernameComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
